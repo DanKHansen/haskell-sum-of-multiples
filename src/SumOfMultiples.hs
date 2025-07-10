2dc8a9a0e8b4dc2977c532f907371f2c29757807
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-10 15:30:33 +0200

    solution

--- src/SumOfMultiples.hs
+++ src/SumOfMultiples.hs
@@ -1,4 +1,7 @@
 module SumOfMultiples (sumOfMultiples) where
 
+import Data.List (nub)
+
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = error "You need to implement this function."
+sumOfMultiples factors limit =
+  sum $ nub $ concatMap (takeWhile (< limit) . (\f -> if f == 0 then [0] else [f, f * 2 ..])) factors
