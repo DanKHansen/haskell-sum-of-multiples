1ac0a4a7ef22ccad6e6837802cac0fea8724c3f8
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-10 15:37:45 +0200

    refactor

--- src/SumOfMultiples.hs
+++ src/SumOfMultiples.hs
@@ -5,4 +5,4 @@ import Data.List (nub)
 sumOfMultiples :: [Integer] -> Integer -> Integer
 sumOfMultiples factors limit = sum . nub $ concatMap multiples factors
   where
-    multiples f = if f == 0 then [] else takeWhile (< limit) [f, f * 2 ..]
+    multiples f = if f == 0 then [] else [f, f * 2 .. limit - 1]
